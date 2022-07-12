9bf2fda6b2ebe745bb52dd36104705f7631fabcb
Hoang <lnhoang1234@gmail.com>
2022-07-12 22:00:10 +0700

    Add DAO.java

---
+++ LibraryManage/src/Inteface/DAO.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Inteface;
+
+/**
+ *
+ * @author hoang
+ */
+public interface DAO {
+    //Them sach
+    public void add();
+    
+    //Xoa sach
+    public void delete();
+    
+    //Cap nhat sach
+    public void update();
+    
+    //Thoat phan mem
+    public void exit();
+    
+    //Tim kiem sach
+    public void search();
+}
