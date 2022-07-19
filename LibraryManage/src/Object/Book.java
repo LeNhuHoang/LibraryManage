e7d1e126badeb5eafa997381ba815be4f527e971
LeNhuHoang <lnhoang1234@gmail.com>
2022-07-19 12:42:21 +0700

    Them class Book.java

--- LibraryManage/src/Object/Book.java
+++ LibraryManage/src/Object/Book.java
@@ -6,8 +6,69 @@ package Object;
 
 /**
  *
- * @author hoang
+ * @author PS23303_Trung
  */
 public class Book {
-    
+
+    private String name, author, type;
+    private int soldAmount;
+    private String description, state;
+
+    public Book(String name, String author, String type, int soldAmount, String description, String state) {
+        this.name = name;
+        this.author = author;
+        this.type = type;
+        this.soldAmount = soldAmount;
+        this.description = description;
+        this.state = state;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getSoldAmount() {
+        return soldAmount;
+    }
+
+    public void setSoldAmount(int soldAmount) {
+        this.soldAmount = soldAmount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
 }
